2daafa5dedf9eb3b50623e5e147e733db9e8c17b
Dejoda <U23216205@utp.edu.pe>
2025-11-13 21:57:53 -0500

    Add files via upload

--- src/main/java/com/proyectoClinica/repository/MedicoEspecialidadRepository.java
+++ src/main/java/com/proyectoClinica/repository/MedicoEspecialidadRepository.java
@@ -3,7 +3,8 @@ package com.proyectoClinica.repository;
 import com.proyectoClinica.model.MedicoEspecialidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 @Repository
 public interface MedicoEspecialidadRepository extends JpaRepository<MedicoEspecialidad, Integer> {
+    List<MedicoEspecialidad> findByMedico_IdMedico(Integer idMedico);
 }
