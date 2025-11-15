a1fdd7b1ffce6dba86d9c7497afdfab6f0d58ffa
Dejoda <U23216205@utp.edu.pe>
2025-11-15 06:12:00 -0500

    Endpoints Get and Put Perfil segun DashboardMedico

--- src/main/java/com/proyectoClinica/repository/MedicoRepository.java
+++ src/main/java/com/proyectoClinica/repository/MedicoRepository.java
@@ -2,6 +2,7 @@ package com.proyectoClinica.repository;
 
 import com.proyectoClinica.model.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -39,5 +40,45 @@ public interface MedicoRepository extends JpaRepository<Medico, Integer> {
 
     Medico findByUsuarioIdUsuario(Integer idUsuario);
 
+
+    /*-------------------------------*/
+    @Query(value = """
+    SELECT 
+        m.id_medico AS idMedico,
+        p.id_persona AS idPersona,
+        p.nombre1,
+        p.nombre2,
+        p.apellido_paterno AS apellidoPaterno,
+        p.apellido_materno AS apellidoMaterno,
+        p.dni,
+        TO_CHAR(p.fecha_nac, 'YYYY-MM-DD') AS fechaNacimiento,
+        p.genero,
+        p.telefono,
+        p.direccion,
+        u.correo,
+        m.colegiatura,
+        e.nombre AS especialidad
+    FROM medico m
+    INNER JOIN persona p ON p.id_persona = m.id_persona
+    LEFT JOIN usuario u ON u.id_usuario = m.id_usuario
+    LEFT JOIN medico_especialidad me ON me.id_medico = m.id_medico
+    LEFT JOIN especialidad e ON e.id_especialidad = me.id_especialidad
+    WHERE m.id_medico = :idMedico
+    """, nativeQuery = true)
+    List<Map<String,Object>> listarPerfilDashboardPorMedico(@Param("idMedico") Integer idMedico);
+/*-------------------------------*/
+    @Modifying
+    @Query(value = """
+    UPDATE medico_especialidad 
+    SET id_especialidad = (
+        SELECT id_especialidad 
+        FROM especialidad 
+        WHERE nombre = :nombreEspecialidad
+    )
+    WHERE id_medico = :idMedico
+    """, nativeQuery = true)
+    void actualizarEspecialidad(
+            @Param("idMedico") Integer idMedico,
+            @Param("nombreEspecialidad") String nombreEspecialidad);
 
 }
